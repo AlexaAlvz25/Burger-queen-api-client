2ced4b6aeee7677b079199b6372f35186068ea39
Fer <ferlaranuz@gmail.com>
2022-11-15 11:29:03 -0600

    add first component login

---
+++ hauss-burgers/public/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Login(){
+    return (
+        <div>
+            <img alt= "flag-color" src= "img/flag-colors.png"/>
+        <img alt= "hauss-logo" src= "img/logo-hauss.png" />
+        <h2>Login</h2>
+       
+        <label for= "user">USER</label>
+        <input type= "text" id= "user"/>
+        <label for= "password">PASSWORD</label>
+        <input type= "text" id= "password"/>
+        <img alt= "burger-login" src= "img/burger-login.png"/>
+        <p>Fast and delicious...</p>
+        <p>Our hamburgers are the best</p>
+        
+    </div>
+    ) 
+}
+
+ 
+root.render(Login())
---
+++ hauss-burgers/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './components/Login';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Login />);
+/* ReactDOM.render(
+   // <React.StrictMode>
+    <Login />,
+    // </React.StrictMode>,
+    document.getElementById('root')
+) */
