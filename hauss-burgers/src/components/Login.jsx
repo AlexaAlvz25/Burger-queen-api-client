e3b38454544ee88e7db67a3ba239b980450f2851
Alexandra Alvarez <alexa.alvarezgtz@gmail.com>
2022-11-22 16:56:25 -0600

    Full integration of user validation by email and password. Includes change of view according to role

--- hauss-burgers/src/components/Login.jsx
+++ hauss-burgers/src/components/Login.jsx
@@ -1,8 +1,8 @@
-import { Link } from 'react-router-dom';
-//import { validateUser } from '../axios.js'
+import { useNavigate } from 'react-router-dom';
+import { validateUser } from '../axios.js'
 
 function Login() {
-    //Obtener los datos del usuario
+    const navigate = useNavigate();
     return (
         <div id="login-container">
             <link href="./login.css" rel="stylesheet" />
@@ -10,17 +10,44 @@ function Login() {
             <img alt="hauss-logo" src="img/logo-hauss.png" className="hauss-logo" />
             <h2 className="login-text">LogIn</h2>
             <div className="login-access" >
-                <label htmlFor="user" className="user-name">USER</label>
-                <input type="text" id="user" />
+                <form onSubmit = {event => {
+                    event.preventDefault();
+                    const emailUser = event.target.email.value;
+                    const userPassword = event.target.password.value;
+                    const endPoint = 'https://6375b37bb5f0e1eb85f6feaa.mockapi.io/api/hb/users';
+                   
+                    if(emailUser == '' || userPassword == '' ){
+                        document.querySelector('h3').style.visibility = 'visible';
+                        document.querySelector('h3').textContent = 'Please enter all fields';
+                    }else{
+                        validateUser(endPoint)
+                        .then((users) => {
+                            users.data.forEach((waiter) => {
+                                if(emailUser == waiter.email && userPassword == waiter.password && waiter.roles == 'waiters') {
+                                    navigate("/waiters");
+                                }else if(emailUser == waiter.email && userPassword == waiter.password && waiter.roles == 'kitchen') {
+                                    navigate("/kitchen")
+                                }else{
+                                    document.querySelector('h3').style.visibility = 'visible';
+                                    document.querySelector('h3').textContent = 'Verificate user & password';
+                                }
+                            })
+                        })
+                    }
+                    }}>
+                <label htmlFor="email" className="user-email">EMAIL</label>
+                <input type="text" id="email" name= "email" />
                 <label htmlFor="password" className="password">PASSWORD</label>
-                <input type="password" id="password" />
-                <Link to='/waiters' className="login-buttom">
-                    <p className='button-Entry'>Entry</p>
+                <input type="password" id="password" name= "password" />
+                <h3></h3>
+                <div className="login-buttom">
+                    <button type= "submit" className='button-Entry'>Entry</button>
                     <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-flame" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <path d="M12 12c2 -2.96 0 -7 -1 -8c0 3.038 -1.773 4.741 -3 6c-1.226 1.26 -2 3.24 -2 5a6 6 0 1 0 12 0c0 -1.532 -1.056 -3.94 -2 -5c-1.786 3 -2.791 3 -4 2z" />
                     </svg>
-                </Link>
+                </div>
+                </form>
             </div>
             <img alt="burger-login" src="img/hamburger_logIn-3.png" id="burger-imageTablet" />
         </div>
--- hauss-burgers/src/components/Waiters.jsx
+++ hauss-burgers/src/components/Waiters.jsx
@@ -1,5 +1,5 @@
 function Waiters() {
   return <h2>Hola soy la vista de meseros</h2>
-}
+  }
 
 export default Waiters
