8d44f932caa18509ec027609590cc89cb18ab92d
Fer <ferlaranuz@gmail.com>
2022-12-06 12:50:23 -0600

    add kitchen component and styles

--- hauss-burgers/src/components/Kitchen.jsx
+++ hauss-burgers/src/components/Kitchen.jsx
@@ -1,5 +1,89 @@
-function Waiters() {
-  return <h2>Hola soy la vista de cocina</h2>
-}
+import './Kitchen.css'
+export default function Kitchen(){
+  return(
+    <div className="kitchen-view">
+      <div className="kitchen-header">
+      <img className= "logo-hauss"src="img/logo-hauss.png" alt="logo-hauss" />
+      <h2 className="kitchen-orders">ORDERS</h2>
+      <img src="img/flag-colors.png" alt="flag-colors" className="flag-colors" />
+      </div>
+      
+      <article className="order">
+        <div className="header-color">
+        <p>TABLE: 2</p>
+        <p>WAITER: FER</p>
+       
+        </div>
 
-export default Waiters
+        <div className="order-resumen">
+        <p>1 x Burger Klein</p>
+        <p>1 x Burger Doppler</p>
+        <button>DONE</button>
+        </div>
+      
+      </article>
+
+      <article className="order">
+        <div className="header-color">
+        <p>TABLE</p>
+        <p>WAITER</p>
+        </div>
+      
+      </article>
+
+      <article className="order">
+        <div className="header-color">
+        <p>TABLE</p>
+        <p>WAITER</p>
+        </div>
+      
+      </article>
+      
+  
+  </div>
+  )
+}import './Kitchen.css'
+export default function Kitchen(){
+  return(
+    <div className="kitchen-view">
+      <div className="kitchen-header">
+      <img className= "logo-hauss"src="img/logo-hauss.png" alt="logo-hauss" />
+      <h2 className="kitchen-orders">ORDERS</h2>
+      <img src="img/flag-colors.png" alt="flag-colors" className="flag-colors" />
+      </div>
+      
+      <article className="order">
+        <div className="header-color">
+        <p>TABLE: 2</p>
+        <p>WAITER: FER</p>
+       
+        </div>
+
+        <div className="order-resumen">
+        <p>1 x Burger Klein</p>
+        <p>1 x Burger Doppler</p>
+        <button>DONE</button>
+        </div>
+      
+      </article>
+
+      <article className="order">
+        <div className="header-color">
+        <p>TABLE</p>
+        <p>WAITER</p>
+        </div>
+      
+      </article>
+
+      <article className="order">
+        <div className="header-color">
+        <p>TABLE</p>
+        <p>WAITER</p>
+        </div>
+      
+      </article>
+      
+  
+  </div>
+  )
+}
